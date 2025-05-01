03aeb183cad53e444074644bc114b6c1b1153001
Spencer Brigham <sbrigham1996@gmail.com>
2025-05-01 00:04:29 -0400

    commit

---
+++ termProject/data/stateColors.js
@@ -0,0 +1,17 @@
+
+const STATE_COLORS = {
+  "Illinois":       "#1f77b4",
+  "Iowa":           "#ff7f0e",
+  "Nebraska":       "#2ca02c",
+  "Minnesota":      "#d62728",
+  "Texas":          "#9467bd",
+  "Indiana":        "#8c564b",
+  "Kansas":         "#e377c2",
+  "California":     "#7f7f7f",
+  "Ohio":           "#bcbd22",
+  "North Dakota":   "#17becf",
+  "Missouri":       "#aec7e8",
+  "Wisconsin":      "#ffbb78",
+  "South Dakota":   "#98df8a",
+  "Washington":     "#ff9896"
+};
