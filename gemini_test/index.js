d8c3afc89eff4c54c5abd948b533b981842e67ae
Spencer Brigham <sbrigham1996@gmail.com>
2025-04-30 23:43:19 -0400

    Update index.js

--- gemini_test/index.js
+++ gemini_test/index.js
@@ -1,6 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
 
-const ai = new GoogleGenAI({ apiKey: "AIzaSyCen8t5xmujBZ9urLfc0GOXuPWRXplXswA" });
+const ai = new GoogleGenAI({ apiKey: "" });
 
 async function main() {
   const response = await ai.models.generateContent({
